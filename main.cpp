121c3d8485647ac0eda470f97b258c2eb18608d4
tarcso <tarnai.csongor@hallgato.ppke.hu>
2022-05-02 14:51:14 +0200

    első hozzáadás

---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include "gordmenu.hpp"
+#include "menuelem.hpp"
+#include "szambeall.hpp"
+#include "pushbutton.hpp"
+#include "Window.hpp"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <functional>
+
+using namespace genv;
+
+const int XX = 400;
+const int YY = 400;
+
+class Ablak : public Window
+{
+
+    GordMenu* g1;
+    GordMenu* g2;
+    PushButton* jobbtn;
+    PushButton* balbtn;
+
+public:
+    Ablak()
+    {
+        g1 = new GordMenu(this, 10, 50, 150, 30, "válassz!", {
+                "elso elem", "masodik elem", "harmadik elem","negyedik elem", "otodik elem", "hatodik elem", "hetedik elem", "nyolcadik elem"
+        }, 5);
+
+        g2 = new GordMenu(this, 240, 50, 150, 30, "válassz!", {"helo", "sza", "hehe", "egysoros", "nice"}, 3);
+
+        jobbtn = new PushButton(this, 180, 50, 20, 30, "->", [this](){
+                std::string s = g1->allapot();
+                g2->hozzaadElem(s);
+                g1->torolElem(s);
+        });
+
+        balbtn = new PushButton(this, 200, 50, 20, 30, "<-", [this](){
+                std::string s = g2->allapot();
+                g1->hozzaadElem(s);
+                g2->torolElem(s);
+        });
+    }
+
+    void ertek_kiir()
+    {
+        std::ofstream logfile("main.log");
+        logfile << g1->allapot();
+        logfile << g2->allapot();
+        logfile.close();
+    }
+
+    void esemeny(const std::string& ki_mondta)
+    {
+        if(ki_mondta == "hello") ertek_kiir();
+    }
+
+
+};
+
+int main()
+{
+    gout.open(400, 400);
+    gout.load_font("LiberationSans-Regular.ttf");
+    Ablak* m = new Ablak;
+    m->event_loop();
+    return 0;
+}
